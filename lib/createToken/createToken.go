ae6fc144e6bed1f9abfa84edbe646e9ad1344df5
Triara <triara.xiii@gmail.com>
2015-07-17 09:49:40 -0700

    feat: added createToken method to create tokens (does't do anything for now, just returns 0)

---
+++ lib/createToken/createToken.go
@@ -0,0 +1,9 @@
+package createToken
+
+type DataToBeTokenized struct {
+	UserId string
+}
+
+func Create (*DataToBeTokenized) (int) {
+	return 0
+}
---
+++ tests/createToken_test.go
@@ -0,0 +1,24 @@
+package createToken_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"strajah-token-go/lib/createToken"
+	"testing"
+)
+
+var _ = Describe("Token creation", func() {
+	It("Should generate a token", func() {
+		data := createToken.DataToBeTokenized{
+			UserId: "user-123",
+		}
+		token := createToken.Create(&data)
+
+		Expect(token).ToNot(BeNil())
+	})
+})
+
+func Test(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Token creation")
+}
